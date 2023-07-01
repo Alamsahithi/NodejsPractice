8b6d0b94861e555df75f8fdbc7a12de34839dc94
Alamsahithi <sahithialam13@gmail.com>
2023-07-01 21:32:06 +0530

    first commit

--- createServer.js
+++ createServer.js
@@ -1,14 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('My name is sahi'); // Replace 'ChatGPT' with your name
-
-  console.log('My name is being logged'); // This will be logged in the console when the server is accessed
+  //res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write("<html>")
+  res.write("<head><title>my first page</title></head>")
+  res.write("<body><p>hello from my nodejs server</p>,/body>"); 
+  res.write("</html>")
+  res.end()
+  //console.log('My name is being logged'); // This will be logged in the console when the server is accessed
 });
 
 const port = 4001;
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+server.listen(port)
---
+++ sendingResponse.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  //res.statusCode = 200;
+  const url=req.url
+  if(url==="/home"){
+    res.write("<html>")
+  res.write("<head><title>welcome home</title></head>")
+  //res.write("<body>form action="/message" method="POST"><input type="text" name="message"></body>"); 
+  res.write("</html>")
+  res.end()
+  }
+  if(url==="/about"){
+    res.write("<html>")
+  res.write("<head><title>welcome to about us page</title></head>")
+  //res.write("<body>form action="/message" method="POST"><input type="text" name="message"></body>"); 
+  res.write("</html>")
+  res.end()
+  }
+  if(url==="/home"){
+  res.setHeader('Content-Type', 'text/html');
+  res.write("<html>")
+  res.write("<head><title>my first page</title></head>")
+  res.write("<body><p>welcome to my node js project</p></body>"); 
+  res.write("</html>")
+  res.end()
+  }
+  //console.log('My name is being logged'); // This will be logged in the console when the server is accessed
+});
+
+const port = 3001
+server.listen(port)
