20ff220f76e385ce30039f121089547e61add821
Alamsahithi <75735501+Alamsahithi@users.noreply.github.com>
2023-07-03 16:55:05 +0530

    Update express1.js

--- express1.js
+++ express1.js
@@ -1,12 +1,20 @@
-const express=require('express')
-const app=express()
-app.use((req,res,next)=>{
-    console.log('in the middleware')
-    next()
-})
-app.use((req,res,next)=>{
-    console.log('in the middleware')
-    res.send('<h1>hello from express js</h1>')
-})
+const express = require('express');
 
-app.listen(3001)
+const app = express();
+
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
+});
+
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
